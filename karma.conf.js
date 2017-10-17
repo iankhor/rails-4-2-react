2dc9d4c9d1cd0f66424244e953270325da157cf6
iankhor <iankhorbc.thecoder@gmail.com>
2017-10-17 22:35:25 +1100

    Setup mocha reporter in karma

--- karma.conf.js
+++ karma.conf.js
@@ -2,32 +2,27 @@ const webpackConfig = require('./config/webpack/test.js')
 
 module.exports = function(config) {
   config.set({
-    basePath: "",
-    frameworks: ["jquery-3.2.1", "jasmine-jquery", "jasmine"],
+    basePath: '',
+    frameworks: ['jasmine'],
     plugins: [
-      "karma-jquery",
-      "karma-jasmine-jquery",
-      "karma-jasmine",
-      "karma-webpack",
-      "karma-chrome-launcher",
-      "karma-coverage-istanbul-reporter" /* optional */,
-      "karma-spec-reporter" /* optional */
+      'karma-jquery',
+      'karma-jasmine-jquery',
+      'karma-jasmine',
+      'karma-webpack',
+      'karma-chrome-launcher',
+      'karma-mocha-reporter',
+      'karma-phantomjs-launcher',
     ],
     files: [ './spec/javascript/**/*.spec.js' ],
     exclude: [],
     webpack: webpackConfig,
-    preprocessors: {'./spec/javascript/**/*.spec.js' : ["webpack"]},
-    mime: { "text/x-typescript": ["ts"] },
-    reporters: ["progress", "coverage-istanbul" /* optional */],
-    coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly', 'text-summary' ],
-      fixWebpackSourcePaths: true
-    } /* optional */,
+    preprocessors: {'./spec/javascript/**/*.spec.js' : ['webpack']},
+    reporters: ['mocha'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ["Chrome"],
+    browsers: ['PhantomJS'],
     singleRun: true
   });
 };
