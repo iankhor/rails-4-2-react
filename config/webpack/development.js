b8b0e288c5579e7627cfe814cf3a0705358f7e09
iankhor <iankhorbc.thecoder@gmail.com>
2017-10-28 19:24:26 +1100

    Add bootstrap initial webpack config'

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,3 +1,16 @@
-const environment = require('./environment')
+const environment = require('./environment');
+const merge = require('webpack-merge')
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const bootstrapEntryPoints= require('./bootstrap.config');
 
-module.exports = environment.toWebpackConfig()
+const isProd = process.env.NODE_ENV === 'production'
+
+const bootstrapConfig = isProd ? bootstrapEntryPoints.prod : bootstrapEntryPoints.dev
+
+const bootstrapWebpackConfig = {
+  entry: {
+    bootstrap: bootstrapConfig
+  }
+}
+
+module.exports = merge(environment.toWebpackConfig(), bootstrapWebpackConfig)
